2d2affdfea8a8982ddb7f61f16f54cb86702f448
TheRealLorenz <lollobellina@gmail.com>
2025-02-05 15:16:43 +0100

    Diminish number of layers

--- keyboards/ferris/sweep/keymaps/bonzo/keymap.c
+++ keyboards/ferris/sweep/keymaps/bonzo/keymap.c
@@ -10,44 +10,30 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x5_2(
                 KC_Q,         KC_W,         KC_E,          KC_R,          KC_T, /*|*/           KC_Y,           KC_U,         KC_I,         KC_O,            KC_P,
-        LALT_T(KC_A), LCTL_T(KC_S), LSFT_T(KC_D),  LGUI_T(KC_F),          KC_G, /*|*/           KC_H,   LGUI_T(KC_J), LSFT_T(KC_K), LCTL_T(KC_L), LALT_T(KC_SCLN),
+        LALT_T(KC_A), LCTL_T(KC_S), LSFT_T(KC_D),  LGUI_T(KC_F),          KC_G, /*|*/           KC_H,   RGUI_T(KC_J), RSFT_T(KC_K), RCTL_T(KC_L), RALT_T(KC_SCLN),
                 KC_Z,         KC_X,         KC_C,          KC_V,          KC_B, /*|*/           KC_N,           KC_M,      KC_COMM,       KC_DOT,         KC_SLSH,
-                                                  LT(3, KC_ESC), LT(1, KC_SPC), /*|*/ LT(2, KC_BSPC), LT(4, KC_ENT)
+                                                         KC_ESC, LT(1, KC_SPC), /*|*/ LT(2, KC_BSPC),  LT(3, KC_ENT)
     ),
 
     [1] = LAYOUT_split_3x5_2(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, /*|*/ KC_HOME, KC_PGDN, KC_PGUP,  KC_END, KC_NO,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, /*|*/ KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_NO,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, /*|*/   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
-                                     KC_NO,   KC_NO, /*|*/  KC_DEL,   KC_NO
+                KC_F12,         KC_F7,         KC_F8,         KC_F9, KC_NO, /*|*/ KC_HOME,         KC_PGDN,         KC_PGUP,          KC_END,         KC_NO,
+        LALT_T(KC_F11), LCTL_T(KC_F4), LSFT_T(KC_F5), LGUI_T(KC_F6), KC_NO, /*|*/ KC_LEFT, RGUI_T(KC_DOWN),   RSFT_T(KC_UP), RCTL_T(KC_RGHT), RALT_T(KC_NO),
+                KC_F10,         KC_F1,         KC_F2,         KC_F3, KC_NO, /*|*/   KC_NO,           KC_NO,           KC_NO,           KC_NO,         KC_NO,
+                                                              KC_NO, KC_NO, /*|*/  KC_DEL,           KC_NO
     ),
 
     [2] = LAYOUT_split_3x5_2(
-        KC_F12, KC_F7, KC_F8, KC_F9,   TO(5), /*|*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_F11, KC_F4, KC_F5, KC_F6, KC_CAPS, /*|*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_F10, KC_F1, KC_F2, KC_F3, CW_TOGG, /*|*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                              KC_NO,  KC_TAB, /*|*/   KC_NO,   KC_NO
+                   KC_1,           KC_2,            KC_3,            KC_4,    KC_5, /*|*/    KC_6,            KC_7,            KC_8,            KC_9,           KC_0,
+        LALT_T(KC_CAPS), LCTL_T(KC_GRV), LSFT_T(KC_TRNS), LGUI_T(KC_TRNS), KC_TRNS, /*|*/ KC_LBRC, RGUI_T(KC_RBRC), RSFT_T(KC_QUOT), RCTL_T(KC_MINS), RALT_T(KC_EQL),
+                CW_TOGG,        KC_TRNS,         KC_TRNS,         KC_TRNS, KC_TRNS, /*|*/   KC_NO,           KC_NO,         KC_TRNS,         KC_TRNS,        KC_BSLS,
+                                                                    KC_NO,  KC_TAB, /*|*/   KC_NO,           KC_NO
     ),
 
     [3] = LAYOUT_split_3x5_2(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, /*|*/ KC_LCBR, KC_RCBR, KC_DQUO, KC_TILD, KC_PIPE,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, /*|*/ KC_LBRC, KC_RBRC, KC_QUOT,  KC_GRV, KC_BSLS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, /*|*/   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                                     KC_NO,   KC_NO, /*|*/   KC_NO,   KC_NO
-    ),
-
-    [4] = LAYOUT_split_3x5_2(
-          KC_NO, KC_7, KC_8,  KC_9,    KC_0, /*|*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_COMM, KC_4, KC_5,  KC_6,  KC_EQL, /*|*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-         KC_DOT, KC_1, KC_2,  KC_3, KC_MINS, /*|*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                             KC_NO,   KC_NO, /*|*/   KC_NO,   KC_NO
-    ),
-
-    [5] = LAYOUT_split_3x5_2(
         KC_BRID, KC_BRIU, KC_NO, KC_NO, KC_NO, /*|*/   KC_NO, KC_MUTE, KC_MPLY,   KC_NO, KC_NO,
           KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, /*|*/ KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_NO,
           KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, /*|*/   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
-                                 TO(0), TO(0), /*|*/   TO(0),   TO(0)
+                                 KC_NO, KC_NO, /*|*/   KC_NO,   KC_NO
     ),
 };
 // clang-format on
